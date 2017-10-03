5a1f6b41db2fae28fd914a09ecef8bfd5a54205b
kabend <kabend@free.fr>
2017-10-03 23:08:07 +0200

    adapted to PostgreSQL

--- server.js
+++ server.js
@@ -1,44 +1,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
-var util = require("uuid");
-
-var TEXTS_COLLECTION = "texts";
-
 var app = express();
-app.use(bodyParser.json());
 
+// postgreSQL
+var pg = require('pg');
+var Transaction = require('pg-transaction');
+ 
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
+app.use(bodyParser.json());
 app.use(express.static(distDir));
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
-});
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
   res.status(code || 500).json({"error": message});
-}
+};
 
 //
 // GET  /api/texts	return all texts
@@ -46,45 +29,59 @@ function handleError(res, reason, message, code) {
 // PUT  /api/texts/:id	update text (with content)
 // 
 app.get("/api/texts", function(req, res) {
-  db.collection(TEXTS_COLLECTION).find({}).toArray(function(err, docs) {
-    if (err) {
-      handleError(res, err.message, "Failed to get texts.");
-    } else {
-      res.status(200).json(docs);
+  const results = [];
+  pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+    if(err) {
+      done(); console.log(err);
+      return res.status(500).json({success: false, data: err});
     }
+    const query = client.query('SELECT * FROM texts;');
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
   });
 });
 
 app.post("/api/texts", function(req, res) {
   var newText = req.body;
-
-  db.collection(TEXTS_COLLECTION).insertOne(newText, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to create new text.");
-    } else {
-      res.status(201).json(doc.ops[0]);
+  pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+    if(err) {
+      done(); console.log(err);
+      return res.status(500).json({success: false, data: err});
     }
+    client.query('INSERT INTO texts(id, content, row, col, docId) values($1, $2, $3, $4, $5);',
+    [newText.id, newText.content, newText.row, newText.col, newText.docId]);
+    done();
   });
+  return res.status(200).json({success: true});
 });
 
 app.put("/api/texts/:id", function(req, res) {
-  var updateDoc = req.body;
 
-  if (!req.body.content) {
-	req.body.content='';
-  }
+  // Grab data from the URL parameters
+  const id = req.params.id;
+  // Grab data from http request
+  const data = {content: req.body.content || '' };
 
   console.log("received put call on texts collection:");
-  console.log(req.params.id);
-  console.log(req.body);
+  console.log(id);
+  console.log(data);
 
-  db.collection(TEXTS_COLLECTION).update({"id":req.params.id}, { $set: updateDoc } , function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to update text");
-    } else {
-      res.status(200).json(updateDoc);
+  pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+    if(err) {
+      done(); console.log(err);
+      return res.status(500).json({success: false, data: err});
     }
+    console.log('UPDATE texts SET content=('+data.content+') WHERE id = ('+id+');');
+    client.query('UPDATE texts SET content=($1) WHERE id = ($2);', [data.content, id]);
+    done();
   });
+  return res.status(200).json({success: true});
+
 });
 
 //
@@ -93,50 +90,77 @@ app.put("/api/texts/:id", function(req, res) {
 //
 
 app.post("/api/rows/:newindex", function(req, res) {
-  var newTextArray = req.body;
 
-  console.log("inserting a new row at index " + req.params.newindex );
-  console.log(newTextArray);
+  // Grab data from the URL parameters
+  const index = req.params.newindex;
+  // Grab data from http request
+  const data = { tArray: req.body };
 
-  // shift all subsequent rows, if any
-  db.collection(TEXTS_COLLECTION).updateMany( { row: { $gte: parseInt(req.params.newindex) } } , { $inc: { row: 1 }  } , function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to update texts during row insertion");
-    } 
-  });
+  console.log("inserting a new row at index " + index );
+  console.log(data.tArray);
 
-  // insert a new row at newindex
-  db.collection(TEXTS_COLLECTION).insert(newTextArray, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to insert a new row.");
-    } else {
-      res.status(200).json(doc);
+  // shift all subsequent rows, if any, then insert a new row at newindex, in one transaction
+  pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+    if(err) {
+      done(); console.log(err);
+      return res.status(500).json({success: false, data: err});
     }
+
+    const tx = new Transaction(client);
+
+    tx.begin();
+
+    const q2 = 'UPDATE texts SET row=row+1 WHERE row >= ($1);';
+    tx.query(q2,[index]);
+
+    const q3 = 'INSERT INTO texts(id,content,row,col) VALUES ($1,$2,$3,$4);';
+    tx.query(q3, [data.tArray[0].id, data.tArray[0].content, data.tArray[0].row, data.tArray[0].col]);
+    tx.query(q3, [data.tArray[1].id, data.tArray[1].content, data.tArray[1].row, data.tArray[1].col]);
+    tx.query(q3, [data.tArray[2].id, data.tArray[2].content, data.tArray[2].row, data.tArray[2].col]);
+    
+    tx.commit();
+
+    done();
   });
-
-
+   
+  return res.status(200).json({success: true});
 });
 
 app.delete("/api/rows/:index", function(req, res) {
 
-  // delete row 
-  db.collection(TEXTS_COLLECTION).deleteMany( { row: { $eq: parseInt(req.params.index) } } , function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to delete a row.");
-    } 
-  });
+  // Grab data from the URL parameters
+  const index = req.params.index;
 
-  // shift all subsequent rows, if any
-  db.collection(TEXTS_COLLECTION).updateMany( { row: { $gt: parseInt(req.params.index) } } , { $inc: { row: -1 }  } , function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to update texts during row deletion");
-    } else {
-      res.status(200).json(doc);
+  console.log("deleting a row at index " + index );
+
+  // delete the row, then shift all further rows, if any, in one transaction
+  pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+    if(err) {
+      done(); console.log(err);
+      return res.status(500).json({success: false, data: err});
     }
+
+    const tx = new Transaction(client);
+
+    tx.begin();
+
+    const q3 = 'DELETE FROM texts WHERE row = ($1);';
+    console.log(q3);
+
+    tx.query(q3,[index]);
+    
+    const q2 = 'UPDATE texts SET row=row-1 WHERE row > ($1);';
+    tx.query(q2,[index]);
+    console.log(q2);
+
+    tx.commit();
+    console.log('commit');
+
+    done();
+    console.log('done');
   });
-
-
+  return res.status(200).json({success: true});
+   
 });
-
 
 
