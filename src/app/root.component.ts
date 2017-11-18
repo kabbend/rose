743287688487697891c6c264aebe8d1421cf018f
kabend <kabend@free.fr>
2017-11-18 12:04:23 +0100

    display section name in header now

--- src/app/root.component.ts
+++ src/app/root.component.ts
@@ -1,4 +1,4 @@
-import { HostListener, Component, OnInit, ElementRef }  from '@angular/core';
+import { HostListener, Component, OnInit, AfterViewChecked, ElementRef }  from '@angular/core';
 
 import { AppComponent }    from './app.component';
 import { CallbackComponent }    from './callback.component';
@@ -6,6 +6,15 @@ import { DocumentService } from './store/document.service';
 
 import { AuthService } from './auth.service';
 import { LoaderService } from './loader.service';
+
+import { Observable }    from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
+
+interface sectionOffset {
+	textid: string;
+	title: string;
+	offset: number;
+};
 
 @Component({
   selector: 'app-root',
@@ -40,14 +49,21 @@ import { LoaderService } from './loader.service';
 
 	<div id="rogse-menu-3" class="ui inverted fixed blue borderless menu" style="position:fixed;top:70px;z-index:+2;">
 	  <!-- SECTIONS DROPDOWN -->
+    	  <div class="item" style="width:20%;height:15px;"> 
     	  <div *ngIf="authService.isAuthenticated() && thereAreSections" class="ui inverted item"> 
 		<section-dd (scroll)="scroll($event)"></section-dd> 
 	  </div>
-    	  <div class="item" style="width:90%;height:20px;"> 
+	  </div>
+    	  <div class="item" style="width:50%;height:15px;"> 
 		<div class="ui container center aligned" style="width:100%;" *ngIf="authService.isAuthenticated() && thereAreDocuments">
 		  <div class="ui transparent input" style="width:100%;">
                      <input style="text-align:center;" type="text" [value]="docTitle" #menubox (keyup.enter)="updateTitle(menubox.value)">
          	  </div>
+		</div>
+	  </div>
+    	  <div class="item" style="width:25%;height:15px;"> 
+		<div class="ui container aligned" style="width:100%;font-size:90%;">
+		     {{secTitle}}
 		</div>
 	  </div>
 	</div>
@@ -72,21 +88,33 @@ import { LoaderService } from './loader.service';
     '../assets/Semantic-UI-CSS-master/semantic.min.css'],
 })
 
-export class RootComponent implements OnInit {
+export class RootComponent implements OnInit, AfterViewChecked {
 
   thereAreSections: boolean = false;	// dynamically check if there are sections to show
   thereAreDocuments: boolean = false;	// dynamically check if there are documents to show
   docTitle: string = "loading...";	// store current document title
+  secTitle: string = '';
   docId: string = "0";			// store current document id
   scrollY: number = 0;			// store current page scroll in px
-  menu_top: number = 0;
+  menu_top: number = 0;			// menu offset (in pixels) from top-of-page, when user scrolls
+  sectionOffsets: sectionOffset[] = [];  // offset of each (sorted) section, so we know in which section we are depending on scroll
  
   //
-  // detect and store page scroll
+  // detect and store page scroll, move menus accordingly
   //
   @HostListener('window:scroll', ['$event'])
   onScroll($event){
+
+    // get current scroll offset
     this.scrollY = $event.target.scrollingElement.scrollTop;
+
+    // find 1st section with this offset 
+    this.secTitle = ''; 
+    for(let i=0;i<this.sectionOffsets.length;i++) {
+	if (this.sectionOffsets[i].offset <= this.scrollY) { this.secTitle = this.sectionOffsets[i].title; }
+	else break;	
+    }
+
     if (this.scrollY > 400) 
 	{ 
 		var offset = (400 - this.scrollY) / 10 ;
@@ -103,8 +131,8 @@ export class RootComponent implements OnInit {
 			elem4.style.marginTop = this.menu_top+'px'; 
 			elem3.style.marginTop = this.menu_top+'px'; 
 		}
-		if (offset < -70 && offset > -120) { elem3.style.marginTop = (-70+(-70-this.menu_top))+'px'; }
-		if (offset < -120) { elem3.style.marginTop = '-20px'; }
+		if (offset < -70 && offset > -115) { elem3.style.marginTop = (-70+(-70-this.menu_top))+'px'; }
+		if (offset < -115) { elem3.style.marginTop = '-25px'; }
 	}
 	else
 	{ 
@@ -120,6 +148,58 @@ export class RootComponent implements OnInit {
 	}
 
   }
+
+  //
+  // store position of each section, so we know where we are when page scrolls
+  //
+  ngOnInit() {
+
+	Observable.combineLatest(
+        this.documentService.getTexts(),
+        this.documentService.getSections(),
+        (t,s) => { var ret = [];
+                   for(var i=0;i<s.length;i++) { ret.push( { text: t.find( t => (t.id == s[i].starttextid) ) || null , section: s[i] }); }
+                   ret = ret.filter( r => r.text != null  ).sort( (a,b) => a.text.row - b.text.row );
+                   return ret.map( r => r.section );
+                 }
+    ).subscribe( 
+		s => { this.sectionOffsets = []; 
+		       s.map( 
+				section => { let id = document.getElementById(section.starttextid);
+					     let y = 0;
+					     if (id) { y = id.getBoundingClientRect().top; console.log("top="+y);}
+					     this.sectionOffsets.push(	{ 
+							textid: section.starttextid, 
+							title: section.title,
+							offset: y
+							} 
+						); 
+					   }	
+			    );
+			console.log("new offsets");
+			console.log(this.sectionOffsets);
+  		     }
+		);
+  }
+
+
+  ngAfterViewChecked() {
+	// little trick here: The Observable for sectionOffsets has been declared, but it might happen that
+	// it has not returned any value yet (array is empty) or it has returned some values but the DOM was not
+	// rendered yet (so all 'top' values are nil). We check for these 2 cases at each AfterViewChecked cycle
+	// and fill-in sectionOffsets array when needed
+	if (this.sectionOffsets.length == 0) return;
+	if (this.sectionOffsets[0].offset == 0) {
+		// not set yet. try to fill it
+		for(let i=0;i<this.sectionOffsets.length;i++) {
+		  let textid = this.sectionOffsets[i].textid;
+		  let id =  document.getElementById(textid);
+		  if (!id) return; // DOM is not rendered yet, abort...
+		  this.sectionOffsets[i].offset = id.getBoundingClientRect().top; 
+		}
+	}
+  }
+
 
   constructor(private documentService : DocumentService, private element: ElementRef, private authService: AuthService, private loaderService: LoaderService ) { 
 
@@ -144,7 +224,9 @@ export class RootComponent implements OnInit {
 	 this.documentService.getDocuments().subscribe( docs => docs.map( d => { 
 			  if (d.current == 'true') { this.docTitle = d.title; this.docId = d.id; this.thereAreDocuments = true; }
 			}) );
+
   }
+
 
   logout() {
 	this.authService.logout();
@@ -152,10 +234,6 @@ export class RootComponent implements OnInit {
 
   login() {
 	this.authService.login();
-  }
-
-  // deprecated 
-  ngOnInit() {
   }
 
   //
@@ -176,6 +254,10 @@ export class RootComponent implements OnInit {
     let elem = document.getElementById('top-of-page');
     if (elem) { elem.scrollIntoView(); }
     this.loaderService.start(); 
+
+    // reset section name
+    this.secTitle = '';
+    this.sectionOffsets = [];
 
     // set selected doc in the store, for all other components
     this.documentService.setDefaultDoc(docId);
